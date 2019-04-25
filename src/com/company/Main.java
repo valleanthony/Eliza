25ba6b95527f09737ba5203ef30ab7e32cbba364
Anthony Valle <valleganthony@gmail.com>
2019-04-25 17:06:43 -0400

    Updating the logic to talk back to user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
 import com.sun.org.apache.xpath.internal.SourceTree;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -25,38 +27,56 @@ public class Main {
         String userInput;
         Boolean keepOpen = true;
         int wordsInArray;
+        int indexOfMyArr = myArr.size();
+        String holder = " ";
+
+
+        //Now going to make the hash map to replace certain words.
+        HashMap<String,String> myMap = new HashMap<String,String>();
+        /*
+        The values entered in the hash map are predetermined.
+         */
+        myMap.put("i","you");
+        myMap.put("me", "you");
+        myMap.put("my","your");
+        myMap.put("am","are");
+
 
         //Initial user message
         System.out.println("Good day. What is your problem? Enter your response here or Q to quit: ");
         userInput= keyboard.nextLine(); // take user input and store it in the var userInput
 
-        String words[] = userInput.split(" "); // This splits up the words in a string and stores them in an array.
-        System.out.println(words.length);//This out puts the qty of words in the array created by the split function.
 
-        for (){
-
-
-        }
-
-
-
-
-
-
-        /*
         //Start the while loop
         while(keepOpen){
             if (userInput.equalsIgnoreCase("q") || userInput.equalsIgnoreCase("I am feeling great")){
                 keepOpen = false;
             }
             else {
-                System.out.println("Why do you say that "+ userInput + "?");
-                userInput = keyboard.nextLine();
-                //if (userInput.split(" ") ==  )
+//                System.out.println("Why do you say that "+ userInput + "?");
+//                userInput = keyboard.nextLine();
+                String words[] = userInput.split(" "); // This splits up the words in a string and stores them in an array.
+                wordsInArray = words.length;//This outputs the qty of words in the array created by the split function.
+
+                for (int i =0; i<wordsInArray; i++){ //This for loop takes all the values in the array made above and stores them in a new array list.
+                    //System.out.println(i); // This prints the index of the array words[]
+                    //System.out.println(words[i]); // This prints the word at the index i (the number above)
+                    //myArr.add(words[i]);// This adds the values from the first array into the arrayList.
+                    //System.out.println(myArr); // this just displays the items inside of the new arraylist.
+                    String answer = "";
+                    if(words[i].equalsIgnoreCase("i")){
+                        words[i]= "you";
+                        System.out.println(words[i]);
+                    }else {
+
+                    }
+                }
+                break;
+                }
+
             }
 
         }
-        */
 
 
 
@@ -65,4 +85,4 @@ public class Main {
 
 
     }
-}
+
